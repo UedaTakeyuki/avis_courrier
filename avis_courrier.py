2c33e762e5a9ff188860fc40c4d33b9fee1773f8
Ueda Takeyuki <ueda@latelierdueda.com>
2018-01-18 22:47:06 +0900

    basic structure

--- avis_courrier.py
+++ avis_courrier.py
@@ -22,30 +22,47 @@ pi3 = True if getrpimodel.model() == "3 Model B" else False
 # GPIO の設定
 GPIO.setmode(GPIO.BOARD)
 
-# 37(out, 1),35(in, 0)
-GPIO.setup(37, GPIO.OUT)
-GPIO.output(37, GPIO.HIGH)
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-# 33(out, 1),31(in, 0)
-# 29(out, 1),27(in, 0)
-# 23(out, 1),21(in, 0)
-# 15(out, 1),13(in, 0)
-#  5(out, 1), 3(in, 0)
-
 # 基盤LED の設定
 l = led.LED()
 l.use(0) # green
 pi3 or l.use(1) # red
 
-if GPIO.input(35):
-  l.off(0)  # green off
-  pi3 or l.on(1) # red on
-else:
-  l.on(0)  # green on
-  pi3 or l.off(1) # red off
-
 def wait():
-  pass
+  # 37(out, 1),35(in, 0)
+  GPIO.setup(37, GPIO.OUT)
+  GPIO.output(37, GPIO.HIGH)
+  GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.add_event_detect(35, GPIO.RISING)
+  # 33(out, 1),31(in, 0)
+  GPIO.setup(33, GPIO.OUT)
+  GPIO.output(33, GPIO.HIGH)
+  GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+  GPIO.add_event_detect(31, GPIO.RISING)
+  # 29(out, 1),27(in, 0)
+  # 23(out, 1),21(in, 0)
+  # 15(out, 1),13(in, 0)
+  #  5(out, 1), 3(in, 0)
+  # 37(out, 1),35(in, 0)
+  while True:
+    try:
+      if GPIO.event_detected(35):
+        l.off(0)
+        time.sleep(1)
+        l.on(0)
+      elif GPIO.event_detected(31):
+        l.off(0)
+        time.sleep(1)
+        l.on(0)
+        time.sleep(1)
+        l.off(0)
+        time.sleep(1)
+        l.on(0)
+    except:
+      info=sys.exc_info()
+      print "Unexpected error:"+ traceback.format_exc(info[0])
+      print traceback.format_exc(info[1])
+      print traceback.format_exc(info[2])
+    time.sleep(1)
 
 def fork():
   pid = os.fork()
